74814d97d3c19d8cf642faf241429ac83b18f265
Mina2023-ST <dinuviminanga8@gmail.com>
2026-06-06 17:33:00 +0530

    Initial project setup

---
+++ Student.java
@@ -0,0 +1,26 @@
+class Student {
+  private String  StudentId;
+  private String StudentName;
+  private double  Marks;
+  
+  Student(int StudentId, String StudentName , int Marks){
+	  this.StudentId=StudentId;
+	  this.StudentName=StudentName;
+	  this.Marks=Marks;
+  }
+  public String getStudentId(){
+	  return StudentId;
+  } 
+  public String getStudentName(){
+	  return StudentName;
+  }
+  public double getMarks(){
+	  return Marks;
+  }
+  public void displayStudent() {
+        System.out.println("Student ID is : " + studentId);
+        System.out.println("Student Name is : " + studentName);
+        System.out.println("Student Marks  : " + Marks);
+    }
+}
+
